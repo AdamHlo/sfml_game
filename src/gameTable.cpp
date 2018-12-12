c49477df9a53e5e63f0207708173f6aefca7af1d
Adam <a.hlozny@gmail.com>
2018-12-12 02:48:07 +0100

    Change style in gameTable

--- src/gameTable.cpp
+++ src/gameTable.cpp
@@ -1,13 +1,9 @@
 #include "gameTable.h"
 #include "gameObject.h"
-#include <string>
 
-const std::string path1 = "data/1B.png";
-const std::string path2 = "data/7.png";
-const std::string path3 = "data/sprite.png";
 
 GameTable::GameTable(){
-  this->player = GameObject(path1);
-  this->enemies.push_back(GameObject(path2));
-  this->enemies.push_back(GameObject(path3));
+  this->player = GameObject("data/1B.png");
+  this->enemies.push_back(GameObject("data/7.png"));
+  this->enemies.push_back(GameObject("data/sprite.png"));
 }
